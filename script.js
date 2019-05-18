b779943a7415536cda3e35aa5052f9d9af07cfba
adag1993 <33051046+adag1993@users.noreply.github.com>
2019-05-18 10:05:42 +0200

    Subida parte JS

--- script.js
+++ script.js
@@ -6,4 +6,26 @@ function corregirTest(){
 	}else{
 		nota=nota-1;
 	}
+	
+	if(document.getElementById("p2").value=="String" || document.getElementById("p2").value=="string" || document.getElementById("p2").value=="STRING"){
+		nota=nota+1;
+	}else{
+		nota=nota-1;
+	}
+	
+	if(document.getElementById("p33").checked && document.getElementById("p35").checked){
+		nota=nota+4;
+	}else if(document.getElementById("p33").checked || document.getElementById("p35").checked){
+		nota=nota+2;
+	}else{
+		nota=nota-1;
+	}
+	
+	var e = document.getElementById("ddlViewBy");
+	if(e.options[e.selectedIndex].value==1){
+		nota=nota+1;
+	}
+
+	
+	
 }
